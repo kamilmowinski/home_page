7b855afc5bc4439bc9860a61f5684f561c4a9be5
Kamil Mówiński <xnasterx@gmail.com>
2015-10-14 08:05:08 +0200

    Added new subpages

--- js/site.js
+++ js/site.js
@@ -1,25 +1,61 @@
 
 var siteApp = angular.module('siteApp', [
-    'ngRoute'
+    'ngRoute',
+    'ngSanitize'
 ]);
 
 siteApp.controller('AppController', function($scope, $http) {
-    $http.get('pages.json').success(function (data) {
-        $scope.pages = data;
+    $http.get('json/news.json').success(function(data) {
+        $scope.news = data;
+    });
+    $http.get('json/informations.json').success(function(data) {
+        $scope.informations = data;
     });
 });
 
 
-siteApp.controller('RouteController', function($scope, $routeParams) {
-    var slug = $routeParams['slug'];
-    $scope.page = $scope.pages[slug];
+siteApp.controller('RouteController', function($scope, $route, $http, $routeParams) {
+  $scope.$route = $route;
 });
 
+siteApp.controller('PublicationController', function($scope, $http, $routeParams) {
+    $http.get('json/publications.json').success(function(data) {
+      $scope.publications = data;
+    });
+});
+
+siteApp.controller('StudentController', function($scope, $http) {
+  $http.get('json/tasks.json').success(function(data) {
+    $scope.tasks = data;
+  });
+  $scope.sortTasks = function(task) {
+    str = task.end.split('-')
+    var date = new Date(str[2], str[1], str[0]);
+    return date;
+  };
+})
 
 siteApp.config(function($routeProvider) {
     $routeProvider
-        .when('/page/:slug', {
-            templateUrl: 'partials/page.html',
-            controller: 'RouteController'})
-        .otherwise({redirectTo: '/page/home'});
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'RouteController',
+            activeTab: 'home'})
+        .when('/about', {
+            templateUrl: 'partials/about.html',
+            controller: 'RouteController',
+            activeTab: 'about'})
+        .when('/contact', {
+            templateUrl: 'partials/contact.html',
+            controller: 'RouteController',
+            activeTab: 'contact'})
+        .when('/publications', {
+            templateUrl: 'partials/publications.html',
+            controller: 'PublicationController',
+            activeTab: 'publications'})
+        .when('/students', {
+            templateUrl: 'partials/students.html',
+            controller: 'StudentController',
+            activeTab: 'students'})
+        .otherwise({redirectTo: '/'});
 });
